176eaf5018e8039330c1fbf6f8b243bd54159878
silentco2 <osamakhalifa333@gmail.com>
2023-05-04 07:46:21 +0300

    pointer follower

--- test.js
+++ test.js
@@ -1,3 +1,6 @@
+var body = document.body,html = document.documentElement;
+var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
+
 function sectionTitleFix(){
     var parent = document.getElementsByClassName("graphic-design")[0]
     var w = parent.getElementsByClassName("branding")[0].offsetWidth;
@@ -6,4 +9,13 @@ function sectionTitleFix(){
     console.log(w/96)
 
 }
+
+function moveHighlight(event){
+    var highlight = document.getElementsByClassName("highlight-pointer")[0]
+    if (event.pageY<height){
+        highlight.style.top = event.pageY + "px"
+        highlight.style.left = event.pageX + "px"
+    }
+}
+document.addEventListener('mousemove', moveHighlight)
 window.onresize = sectionTitleFix
